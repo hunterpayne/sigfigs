e864088af01832b28dca2ee7bd4bbdb7d698c3f5
hunterpayne <hunterpayne2001@yahoo.com>
2019-03-12 17:19:54 -0700

    Cleaning up imports

--- src/test/scala/org/sigfigs/SigfigsSuiteTest.scala
+++ src/test/scala/org/sigfigs/SigfigsSuiteTest.scala
@@ -4,14 +4,12 @@ package org.sigfigs
 import org.scalatest._
 import org.scalatest.events._
 
-import org.junit.{ BeforeClass, Test }
-import org.junit.runner.{ RunWith, Result, Runner, Description }
-import org.junit.runners.{ Suite => JUnit4Suite, ParentRunner }
-import org.junit.runners.model.{ RunnerBuilder, Statement }
-import org.junit.runner.notification.{ RunNotifier, Failure }
+import org.junit.BeforeClass
+import org.junit.runner.RunWith
+import org.junit.runners.{ Suite => JUnit4Suite }
 
 import com.carrotgarden.sjs.junit.{ 
-  LinkerImpl, SuiteSetupImpl, TestInit, ScalaJS_Suite }
+  LinkerImpl, SuiteSetupImpl, TestInit, ScalaJS_ScalatestSuite }
 import SigfigsSuiteTest._
 
 object Linker extends LinkerImpl
@@ -48,45 +46,3 @@ object SigfigsSuiteTest {
   class TimeSuite
 }
 
-class ScalaJS_ScalatestSuite(klaz: Class[_], builder: RunnerBuilder) 
-    extends JUnit4Suite(klaz, builder) {
-
-  import collection.JavaConversions._
-
-  protected override def getChildren(): java.util.List[Runner] =
-    klaz.getAnnotationsByType(classOf[JUnit4Suite.SuiteClasses]).flatMap {
-      _.value.map { clz => {
-        val suite = clz.newInstance().asInstanceOf[Suite]
-        new ScalatestRunner(clz.asInstanceOf[Class[Suite]], suite) } } }.toSeq
-
-  class ScalatestRunner(clazz: Class[Suite], scalatestSuite: Suite)
-      extends ScalaJS_Suite(clazz, Seq(this)) {
-
-    lazy val desc =
-      Description.createTestDescription(clazz, scalatestSuite.suiteName)
-
-    override def run(notifier: RunNotifier): Unit = {
-      val rep = new Reporter {
-        def apply(event: Event): Unit = event match {
-          case ts: TestStarting => notifier.fireTestStarted(
-            Description.createTestDescription(clazz, ts.testText))
-          case ts: TestSucceeded => notifier.fireTestFinished(
-            Description.createTestDescription(clazz, ts.testText))
-          case ti: TestIgnored => notifier.fireTestIgnored(
-            Description.createTestDescription(clazz, ti.testText))
-          case tf: TestFailed =>
-            notifier.fireTestFailure(new Failure(
-              Description.createTestDescription(clazz, tf.testText),
-              tf.throwable.getOrElse(new Exception(tf.message))))
-          case _ => // do nothing
-        }
-      }
-      scalatestSuite.run(None, Args(reporter = rep))
-    }
-
-    override def getDescription(): Description = desc
-    override def testCount(): Int = scalatestSuite.testNames.size
-
-    override def toString(): String = s"ScalatestRunner[${clazz.getName()}]"
-  }
-}
